76a295c1abde223552a52627952166f1fdc051ee
Andres <Jimerueda26@gmail.com>
2023-02-28 15:20:23 -0500

    Se modifico el setings del proyecto principal django, escribiendo correctamente la configuracion de la base de datos postgres

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         #"ENGINE": "django.db.backends.sqlite3",
         #"NAME": BASE_DIR / "db.sqlite3",
         'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/postgres',
+            default='postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
         )
 
